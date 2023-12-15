1ba47f0ddae614b8b3d38d15f4ac07a3632f7c75
epata94 <hklee0724@gmail.com>
2023-12-15 13:04:38 +0900

    .

--- Cal.java
+++ Cal.java
@@ -1,12 +1,6 @@
 import java.util.Scanner;
-<<<<<<< HEAD
 // 2
 public class Calculator { 
-    public static void main(String[] args) { 
-=======
-// 1
-public class Calculator { 
     public static void main(String[] args) {
->>>>>>> 92ddf5db7de6aa2b5187815ee103d77c420c9ba3
     }
 }
